f7b3d91a83a5c8907dd3d2e5cc7eb60eff3cbcca
David Ramirez Espinoza <ing.davidramirezespinoza@gmail.com>
2026-03-11 16:13:39 -0600

    corriegiendoAlertas2

--- src/App.tsx
+++ src/App.tsx
@@ -408,7 +408,7 @@ export default function App() {
         racks.map((rack, index) => ({
           id: rack.id,
           name: rack.name,
-          type: "RACK",
+          type: "RACK", 
           rack: rack.name,
           position: index
         }))
